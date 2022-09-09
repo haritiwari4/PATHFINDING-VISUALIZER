3c777466c88f53e7dc42f316462dd24d818f9caa
haritiwari4 <haritiwariofficial@gmail.com>
2022-09-09 17:30:35 +0530

    Updates

---
+++ precache-manifest.f39d7e0a900999ee0b76921b47fbcfe4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96fb7cf8bb32eea86754b6026e9fceb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e42fcb5f17c093a2916b",
+    "url": "/static/css/main.6423520f.chunk.css"
+  },
+  {
+    "revision": "a630968cba511403d833",
+    "url": "/static/js/2.8e683c52.chunk.js"
+  },
+  {
+    "revision": "e42fcb5f17c093a2916b",
+    "url": "/static/js/main.d79d7b2c.chunk.js"
+  },
+  {
+    "revision": "512a4f888a379d4d7645",
+    "url": "/static/js/runtime-main.1638fc71.js"
+  }
+]);
